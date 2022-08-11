9b0b7215cf529a6157ed6222428dea43f2bde569
fortvivlan <jahil.kematian@gmail.com>
2022-08-11 18:11:27 +0300

    add version 1.0

---
+++ gicrdoc.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import pandas as pd
+from docx import Document
+
+
+def docwrite(data, name):
+    doc = Document()
+    for i, row in data.iterrows():
+        if str(row['left']) != 'nan':
+            p = doc.add_paragraph(f"{i}\t{row['left']}")
+        else:
+            p = doc.add_paragraph(f'{i}\t')
+        p.add_run(f" {row['result']} ").bold = True
+        if str(row['right']) != 'nan':
+            p.add_run(str(row['right']))
+        doc.add_paragraph(f"Дата создания текста: {row['year']}")
+        doc.add_paragraph(f"Год рождения автора: {row['birth']}")
+        doc.add_paragraph(f"Место жительства автора: {row['loc']}")
+        doc.add_paragraph(' ')
+    doc.save(f'docs/{name}.docx')
+
+if not os.path.exists('docs'):
+    os.mkdir('docs')
+
+if not os.path.exists('data'):
+    os.mkdir('data')
+    print('Put your xslx files into data folder')
+    sys.exit()
+
+files = os.listdir('data')
+
+for name in files:
+    if name.endswith('.xlsx'):
+        data = pd.read_excel(f'data/{name}')
+        data = data.drop_duplicates(subset='left', keep='first')
+        docwrite(data, name[:-5])
